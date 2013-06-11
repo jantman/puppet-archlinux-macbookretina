13f45e667d5f9bdb6dae91f12746d97447a84f32
Jason Antman <jason@jasonantman.com>
2013-06-11 15:37:37 -0400

    add screen to base

--- manifests/base.pp
+++ manifests/base.pp
@@ -12,12 +12,15 @@
 # Sample Usage:
 #
 class puppet-archlinux-macbookretina::base {
-  package {'lynx': ensure => present, }
+  package {'links': ensure => present, }
+  package {'lynx': ensure => absent, }
   package {'lsb-release': ensure => present, }
   package {'dmidecode': ensure => present, }
   package {'macfanctld-git': ensure => present, }
   package {'alsa-utils': ensure => present, }
   package {'ttf-dejavu': ensure => present, }
+  package {'screen': ensure => present, }
+  
 
   # base config files
   file {'/etc/makepkg.conf':
