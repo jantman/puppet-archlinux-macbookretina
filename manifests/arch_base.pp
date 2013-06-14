5d8e516c73ad769a08c26e16c3daa2df8be13bb4
Jason Antman <jason@jasonantman.com>
2013-06-14 09:19:50 -0400

    include mosh, since I'm on a crappy wifi connection, and it's wonderful

--- manifests/arch_base.pp
+++ manifests/arch_base.pp
@@ -25,7 +25,7 @@ class puppet-archlinux-macbookretina::arch_base {
   package {'alsa-utils': ensure => present, }
   package {'ttf-dejavu': ensure => present, }
   package {'screen': ensure => present, }
-  
+  package {'mosh': ensure => present, }  
 
   # base config files
   file {'/etc/makepkg.conf':
