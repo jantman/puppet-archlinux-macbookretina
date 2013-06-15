0474aa3297efc29f2a2b13e60251049ed60a606a
Jason Antman <jason@jasonantman.com>
2013-06-15 14:46:57 -0400

    adding pdnsd and unaltered sample config file

---
+++ manifests/pdnsd.pp
@@ -0,0 +1,31 @@
+# Class: puppet-archlinux-macbookretina::pdnsd
+#
+# Install and configute pdnsd for persistent local dns caching.
+#
+# Parameters:
+#
+# Actions:
+#   - Install pdnsd
+#
+# Requires:
+#
+# Sample Usage:
+#
+class puppet-archlinux-macbookretina::pdnsd {
+
+  package {'pdnsd':
+    ensure => present,
+  }
+
+  file {'/etc/pdnsd.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/puppet-archlinux-macbookretina/pdnsd.conf',
+    require => Package['pdnsd'],
+  }
+
+  # service laptop-mode
+
+}
