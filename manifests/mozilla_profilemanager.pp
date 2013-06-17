e1b20ffab27c41eec45d816f3142b1bd90b6e018
Jason Antman <jason@jasonantman.com>
2013-06-17 08:54:44 -0400

    mozilla profilemanager installed via extracting tarball. need to make it an AUR package once I remember my GPG key passphrase...

--- manifests/mozilla_profilemanager.pp
+++ manifests/mozilla_profilemanager.pp
@@ -1,6 +1,6 @@
 # Class: puppet-archlinux-macbookretina::mozilla_profilemanager
 #
-# Install Mozilla ProfileManager for Firefox - https://developer.mozilla.org/en-US/docs/Profile_Manager
+# Install Mozilla ProfileManager for Firefox as an ugly binary untar
 #
 # Parameters:
 #
@@ -15,24 +15,25 @@
 class puppet-archlinux-macbookretina::mozilla_profilemanager {
 
   # To Do: make this a real package and get it on AUR. Get rid of this ugliness
-#  file { "/tmp/mozilla-profilemanager":
-#    ensure => directory,
-#    owner  => 'root',
-#    group  => 'root',
-#  } ->
-#  exec { "wget-profilemanager":
-#    command => "/bin/wget -O /tmp/mozilla-profilemanager/profilemanager.linux64.tar.gz ftp://ftp.mozilla.org/pub/mozilla.org/utilities/profilemanager/1.0/profilemanager.linux64.tar.gz",
-#    creates => "/root/temp/mozilla-profilemanager/profilemanager.linux64.tar.gz",
-#    cwd     => "/root/temp/mozilla-profilemanager/",
-#  } ->
-#  exec { "untar-profilemanager":
-#    command => "/bin/tar -xzvf /tmp/mozilla-profilemanager/profilemanager.linux64.tar.gz",
-#    creates => "/usr/local/bin/profilemanager/profilemanager-bin",
-#    cwd     => "/usr/local/bin",
-#  } -> 
-#  file { "/usr/share/applications/mozilla-profilemanager.desktop":
-#    ensure => present,
-#    source => 'puppet:///modules/puppet-archlinux-macbookretina/mozilla-profilemanager.desktop',
-#  }
+  file { "/tmp/mozilla-profilemanager":
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+  } ->
+  exec { "wget-profilemanager":
+    command => "/bin/wget -O /tmp/mozilla-profilemanager/profilemanager.linux64.tar.gz ftp://ftp.mozilla.org/pub/mozilla.org/utilities/profilemanager/1.0/profilemanager.linux64.tar.gz",
+    creates => "/tmp/mozilla-profilemanager/profilemanager.linux64.tar.gz",
+    cwd     => "/tmp/mozilla-profilemanager/",
+    require => Package['wget'],
+  } ->
+  exec { "untar-profilemanager":
+    command => "/bin/tar -xzvf /tmp/mozilla-profilemanager/profilemanager.linux64.tar.gz",
+    creates => "/usr/local/bin/profilemanager/profilemanager-bin",
+    cwd     => "/usr/local/bin",
+  } -> 
+  file { "/usr/share/applications/mozilla-profilemanager.desktop":
+    ensure => present,
+    source => 'puppet:///modules/puppet-archlinux-macbookretina/mozilla-profilemanager.desktop',
+  }
 
 }
