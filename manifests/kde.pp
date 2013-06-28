616a02108fe9499c20c9eb5476827f214e7c0b56
Jason Antman <Jason.Antman@coxinc.com>
2013-06-28 13:51:18 -0400

    adding kdemultimedia-kmix for mixer

--- manifests/kde.pp
+++ manifests/kde.pp
@@ -20,7 +20,7 @@ class puppet-archlinux-macbookretina::kde {
     require => Package['kdebase-workspace'],
   }
 
-  $kde_packages = ['kdebase', 'kdebase-workspace', 'kdeutils-kwallet', 'kdenetwork-krdc', 'rdesktop'  ]
+  $kde_packages = ['kdebase', 'kdebase-workspace', 'kdeutils-kwallet', 'kdenetwork-krdc', 'rdesktop', 'kdemultimedia-kmix' ]
 
   package {$kde_packages:
     ensure => present,
