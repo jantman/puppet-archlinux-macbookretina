38d35957ea6024e53b10bc6e0cb60683ce8f52e7
Jason Antman <Jason.Antman@coxinc.com>
2013-07-09 13:57:16 -0400

    add whois

--- manifests/jantman_packages.pp
+++ manifests/jantman_packages.pp
@@ -30,6 +30,7 @@ class puppet-archlinux-macbookretina::jantman_packages {
   package {'skype': ensure => present, }
   package {'teamspeak3': ensure => present, }
   package {'teamviewer': ensure => present, }
+  package {'whois': ensure => present, }
 
   # misc dev/editing
   package {'meld': ensure => present, }
