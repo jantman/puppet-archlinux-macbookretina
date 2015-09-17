0f53d55e3abfab53a0f79e540a1001880c396cd6
Jason Antman <jason@jasonantman.com>
2015-09-17 07:03:08 -0400

    mac_keyboard

---
+++ spec/acceptance/classes/mac_keyboard_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper_acceptance'
+
+describe 'archlinux_macbookretina::mac_keyboard class' do
+
+  context 'default parameters' do
+    # Using puppet_apply as a helper
+    it 'should work with no errors' do
+      pp = <<-EOS
+      class { 'archlinux_macbookretina::mac_keyboard': }
+      EOS
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    describe file('/etc/modprobe.d/apple.conf') do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode 644 }
+      its(:content) { should match /options hid_apple fnmode=2/ }
+    end
+  end
+end
---
+++ spec/classes/mac_keyboard_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'archlinux_macbookretina::mac_keyboard' do
+  let(:facts) {{
+    :osfamily        => 'Archlinux',
+    :operatingsystem => 'Archlinux',
+    :productname     => 'MacBookPro11,4',
+  }}
+
+  describe "creates file" do
+      it { should compile.with_all_deps }
+      it { should contain_file('/etc/modprobe.d/apple.conf')
+                   .with({
+                           :ensure => 'present',
+                           :owner  => 'root',
+                           :group  => 'root',
+                           :mode   => '0644',
+                           :source => 'puppet:///modules/archlinux_macbookretina/apple.conf',
+                         })
+      }
+  end
+end
