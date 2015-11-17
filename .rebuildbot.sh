cdd6309ebeee371fdc5625f715b233d197fc67f4
Jason Antman <jason@jasonantman.com>
2015-11-16 21:58:39 -0500

    rebuildbot updates

--- .rebuildbot.sh
+++ .rebuildbot.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -ex
 
+set +x
 . ~/.rvm/scripts/rvm
 rvm use $(cat .ruby-version)
 rvm info
 which ruby
 ruby -v
+set -x
 
 bundle install --path vendor
+bundle exec rake spec_prep
 bundle exec rake beaker
