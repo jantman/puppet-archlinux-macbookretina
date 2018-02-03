961e3bc8e8a5fda7d8ca8b7eb333f60724ba5788
Jason Antman <jason@jasonantman.com>
2018-02-02 21:06:21 -0500

    WIP acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -16,10 +16,12 @@ RSpec.configure do |c|
       on h, 'pacman -Syu --noconfirm' unless ENV['BEAKER_provision'] == 'no'
       run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
       install_module_dependencies_on(h)
-      on(host, puppet('module', 'install', 'jantman-archlinux_workstation'))
+      on h, puppet('module', 'install', 'jantman-archlinux_workstation')
       # on ArchLinux, install_module_on(hosts) installs in /etc/puppet/modules
       # instead of /etc/puppetlabs/code/modules
       copy_module_to(h, source: proj_root, module_name: 'archlinux_macbookretina', target_module_path: '/etc/puppetlabs/code/modules')
+      on h, 'mkdir -p /etc/facter/facts.d'
+      scp_to(h, File.join(proj_root, 'spec', 'acceptance_test_fact_overrides.yaml'), '/etc/facter/facts.d/acceptance_test_overrides.yaml')
     end
   end
 end
